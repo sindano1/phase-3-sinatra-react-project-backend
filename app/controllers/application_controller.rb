39e6132004852f392197f944eed49e928fa7b2dd
Sarah Indano <sarah.indano@gmail.com>
2022-04-19 11:26:12 -0400

    lesson post request created, assigns trainer to trainer_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,13 @@ class ApplicationController < Sinatra::Base
   
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
+  # get "/" do
+  #   { message: "Good luck with your project!" }.to_json
+  # end
 
+
+
+  ########### Trainers ##############
   get "/trainers" do
     Trainer.all.to_json(include: :lessons)
   end
@@ -16,6 +19,10 @@ class ApplicationController < Sinatra::Base
     Trainer.find(params[:id]).to_json(include: :lessons)
   end
 
+
+
+
+############# Lessons #############
   get "/lessons" do
     Lesson.all.to_json
   end
@@ -24,6 +31,16 @@ class ApplicationController < Sinatra::Base
     Lesson.find(params[:id]).to_json
   end
 
+  post "/lessons" do
+    new_lesson = {**params, "trainer" => Trainer.find_by(name: params[:trainer])}
+    Lesson.create(new_lesson).to_json
+  end
+  # Above, posts a lesson while recieving a trainer as a string and assigns appropriate value
+
+
+
+  ############### Dogs ############
+
   get "/dogs" do
     Dog.all.to_json(include: :owners)
   end
@@ -31,6 +48,11 @@ class ApplicationController < Sinatra::Base
   get "/dogs/:id" do
     Dog.find(params[:id]).to_json(include: :owners)
   end
+
+
+
+
+  ########### Owners ###########
 
   get "/owners" do
     Owner.all.to_json(include: :dogs)
